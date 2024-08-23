e47ae4bc3e5a5fcff674ebf430b582813a9278b2
Dmitry Ponyatov <dponyatov@gmail.com>
2024-08-23 20:21:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,13 @@
 import pygame
+import sys
+from pygame.locals import *
 
-
-print("Hello")
-
-
-number = random.randint(1, 20)
-spam = 42
-print('Hello world!')
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True:  # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
