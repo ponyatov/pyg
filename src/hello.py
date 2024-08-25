1319db3c9e9f48873478af9f3c55da4cad4e9626
Dmitry Ponyatov <dponyatov@gmail.com>
2024-08-25 04:26:24 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/hello.py
+++ src/hello.py
@@ -2,12 +2,21 @@ import pygame
 import sys
 from pygame.locals import *
 
+W = 240
+H = 320
+
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((W, H))
 pygame.display.set_caption('Hello World!')
+
 while True:  # main game loop
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        match event.type:
+            case pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            case pygame.MOUSEBUTTONDOWN:
+                match event.button:
+                    case 3:
+                        print(event)
     pygame.display.update()
