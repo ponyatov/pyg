61704166caca99e7cb6dfad006b0f9dab2834e83
Dmitry Ponyatov <dponyatov@gmail.com>
2024-08-25 22:29:03 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/hello.py
+++ src/hello.py
@@ -44,10 +44,11 @@ def render():
     top().render()
     status().render()
     pygame.draw.polygon(disp, GREEN, (
-        (146, top.rect.bottom + top.H),
+        (top.rect.centerx, top.rect.bottom + top.H),
         (screen.width - top.H, 106),
-        (screen.width / 5 * 4, 277),
-        (56, 277), (0 + top.H, 106)
+        (screen.width / 5 * 4, screen.height / 2),
+        (56, 277),
+        (0 + top.H, 106)
     ))
 
 
